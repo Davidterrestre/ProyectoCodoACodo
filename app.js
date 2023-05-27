82bb92035eb10dea642ab26efa8556a4077e9297
Mauro <mauroezet@gmail.com>
2023-05-27 00:56:14 -0300

    ENVIO TRES MODIFICACIONES

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
-let endpoint = 'https://api.binance.com/api/v3/ticker/price'
-fetch(endpoint)
-    .then(respuesta => respuesta.json())
-    .then(datos => mostrarData(datos))
-    .catch(e => console.log(e))
+const symbols = ['BTCUSDT', 'ETHUSDT', 'BNBUSDT', 'LTCUSDT', 'XRPUSDT', 'ADAUSDT', 'DOTUSDT', 'LINKUSDT', 'BCHUSDT', 'XLMUSDT', 'DOGEUSDT', 'ETCUSDT'];
 
+const baseUrl = 'https://api.binance.com/api/v3';
 
-const mostrarData = (data) => {
-    //console.log(data)
-    let body = ''
-    for (let i = 0; i < data.length; i++) {
-        body += `<tr><td>${data[i].symbol}</td><td>${data[i].price}</td></tr>`
-    }
-    document.getElementById('data').innerHTML = body
+const tabla1Symbols = symbols.slice(0, Math.ceil(symbols.length * 0.1));
+const tabla2Symbols = symbols.slice(Math.ceil(symbols.length * 0.1));
+
+tabla1Symbols.forEach(symbol => {
+  const url = `${baseUrl}/ticker/price?symbol=${symbol}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      mostrarData(data, 'tabla1');
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+tabla2Symbols.forEach(symbol => {
+  const url = `${baseUrl}/ticker/price?symbol=${symbol}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      mostrarData(data, 'tabla2');
+    })
+    .catch(error => {
+      console.error(error);
+    });
+});
+
+const mostrarData = (data, tablaId) => {
+  const table = document.getElementById(tablaId);
+  const tbody = table.querySelector('tbody');
+  const row = document.createElement('tr');
+  
+  const symbolCell = document.createElement('td');
+  symbolCell.textContent = data.symbol;
+  
+  const priceCell = document.createElement('td');
+  priceCell.textContent = data.price;
+  
+  row.appendChild(symbolCell);
+  row.appendChild(priceCell);
+  tbody.appendChild(row);
 }
